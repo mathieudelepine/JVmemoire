aab379cc6b6bf0d7718551335d24204b3562ece6
mathieudelepine <mathieu.delepine@mmibordeaux.com>
2020-11-26 16:25:02 +0100

    style boutton

--- js/main.js
+++ js/main.js
@@ -6,4 +6,39 @@ window.onload = () => {
 	navigator.serviceWorker
 			 .register('./sw.js'); // le cas échant on enregistre notre gestionnaire
 	}
+
+	document.getElementById("moncercle1")
+}
+
+// class circle {
+// 	constructor(etat) {
+// 		this.etat = etat;
+// 	}
+// 	afficherProprietes() {
+// 		console.log(this.etat);
+// 	}
+// }
+
+// // créez 5 cercles
+
+// var circle1 = new circle(0);// appel au constructeur
+// circle1.afficherProprietes();
+
+// var circle2 = new circle(0);// appel au constructeur
+// circle2.afficherProprietes();
+
+// var circle3 = new circle(0);// appel au constructeur
+// circle3.afficherProprietes();
+
+// var circle4 = new circle(0);// appel au constructeur
+// circle4.afficherProprietes();
+
+// var circle5 = new circle(0);// appel au constructeur
+// circle5.afficherProprietes();
+
+var game = {
+	count: 0,
+	possibilities: ['#purple','#blue', '#red', '#orange'],
+	currentGame: [],
+	player: [],
 }
